77f02da82c28d2d12bd67c7c547bd7b7cde861fe
aimee-liang <aimeeeliang@gmail.com>
2020-10-14 18:37:55 -0400

    Second deliverable complete

--- src/index.js
+++ src/index.js
@@ -17,6 +17,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const quoteList = document.querySelector("#quote-list")
         const quoteListLi = document.createElement("li")
         quoteListLi.classList.add("quote-card")
+        // add a data set id
         quoteListLi.innerHTML = `
         <blockquote class="blockquote">
             <p class="mb-0">${quote.quote}</p>
@@ -32,9 +33,36 @@ document.addEventListener('DOMContentLoaded', () => {
     /* Deliverable 2: Submitting the form creates a new quote and adds it to the list of quotes without having to refresh the page.*/
 
     const submitHandler = () => {
+        const form = document.querySelector("#new-quote-form")
+        form.addEventListener("submit", (e) => {
+            e.preventDefault();
+            const form = e.target
 
+            const quote = form.quote.value
+            const author = form.author.value
+
+            const newQuote = {quote: quote, author: author, likes: 0}
+
+            const options = {
+                method: "POST",
+                headers: {
+                    "content-type": "application/json",
+                    "accept": "application/json"
+                },
+                body: JSON.stringify(newQuote)
+            }
+
+            form.reset()
+
+            fetch(baseUrl, options)
+                .then(response => response.json())
+                .then(quote => {
+                    renderQuote(quote)
+                })
+        })
     }
 
     getQuotes();
+    submitHandler();
 
 })
